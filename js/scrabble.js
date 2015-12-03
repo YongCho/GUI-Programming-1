58f84b996575b76eceae0299e269c5f580a83c36
YongCho <yongjec@gmail.com>
2015-12-03 16:45:56 -0500

    Add empty css and js files for assignment 9

---
+++ js/scrabble.js
@@ -0,0 +1,12 @@
+/*
+Author: Yong Cho
+Email: Yong_Cho@student.uml.edu
+Created: 12/3/2015
+File Description: JavaScript for assignment 9 page.
+*/
+
+"use strict";
+
+$(window).load(function() {
+
+});
